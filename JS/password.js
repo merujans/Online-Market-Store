092dae74a12428985f698d4c0891608e23cb0829
merujans <127285270+merujans@users.noreply.github.com>
2024-12-23 11:15:35 +0530

    Add files via upload

---
+++ JS/password.js
@@ -0,0 +1,21 @@
+function checkpassword() {
+	if (document.getElementById("pws").value == getElementById("comfrompws").value) {
+		alert("success");
+		return true;
+	}
+	else {
+		alert("Not success");
+		return fales;
+	}
+}
+	function enabelbutton() {
+		if (document.getElementById("chek").value == getElementById("sub").value) {
+
+		}
+
+
+		else {
+			document.getElementById("sub").disable true;
+		}
+
+	}
